abf8bdaf2d9abb1a0e31d14091104e97eb074703
Evgeniy <evgeniy@evgeniy.ru>
2022-06-04 23:50:31 +0300

    first

---
+++ course/js/007.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('hello');
+
+// const resoult = confirm('Вы тут?');
+// console.log(resoult);
+
+// const answer = prompt('Вам есть 18', '');
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя?');
+answers[1] = prompt('Ваше фамилия?');
+answers[2] = prompt('Ваше Отчество?');
+
+document.write(answers);
---
+++ course/js/008.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl/${category}/5`);
+
+const name = 'Jonh';
+
+alert(`Твое имя  ${name}`);
